b5256f4d9f9a3b799bd9f73b39ec86c9cfe52f47
tejasteju <tejastejascp@gmail.com>
2023-10-26 16:23:08 +0530

    add

---
+++ comments.php
@@ -0,0 +1,12 @@
+<?php
+$a="Technerds";
+
+//echo("$a<br>");
+echo("$a<br>");
+print"technerds<br> ";
+print"technerds<br>";
+echo"$a<br>";
+print"technerds"."<br>".$a."<br>".$a;
+echo("technerds"."<br>".$a);
+
+?>
